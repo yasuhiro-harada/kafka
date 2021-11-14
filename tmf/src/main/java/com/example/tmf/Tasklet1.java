f1ad79f9b1923021f798b0bacdf4dce1e648d6b2
yasuhiro-harada <73407637+yasuhiro-harada@users.noreply.github.com>
2021-11-14 21:51:08 +0900

    produce+consume

--- tmf/src/main/java/com/example/tmf/Tasklet1.java
+++ tmf/src/main/java/com/example/tmf/Tasklet1.java
@@ -7,12 +7,38 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.IntStream;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 @Component
 @StepScope
 public class Tasklet1 implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contoribution, ChunkContext cuhnkcontext) throws Exception{
-        System.out.println("tasklet1!!");
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        try (KafkaProducer<String, String> producer =
+                     new KafkaProducer<>(properties, new StringSerializer(), new StringSerializer())) {
+
+            IntStream
+                    .rangeClosed(1, 10)
+                    .forEach(i -> {
+                        try {
+                            producer.send(new ProducerRecord<>("topic", "key" + i, "value" + i)).get();
+                            System.out.println("produce : value" + i);
+                        } catch (InterruptedException | ExecutionException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+
+        }
         return RepeatStatus.FINISHED;
     }
 }
--- tmf/src/main/java/com/example/tmf/Tasklet2.java
+++ tmf/src/main/java/com/example/tmf/Tasklet2.java
@@ -7,12 +7,44 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+
 @Component
 @StepScope
 public class Tasklet2 implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contoribution, ChunkContext cuhnkcontext) throws Exception{
-        System.out.println("tasklet2!!");
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-consumer");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer =
+                new KafkaConsumer<>(properties, new StringDeserializer(), new StringDeserializer());
+
+        List<ConsumerRecord<String, String>> received = new ArrayList<>();
+
+        consumer.subscribe(Arrays.asList("topic"));
+
+        while (received.size() < 10) {
+            Duration duration = Duration.ofMillis(1000);
+            ConsumerRecords<String, String> records = consumer.poll(duration);
+
+            records.forEach(received::add);
+            System.out.println("consume : value");
+        }
+        consumer.close();
         return RepeatStatus.FINISHED;
     }
 }
